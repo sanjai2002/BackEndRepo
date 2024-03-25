7de0164083897c354aab0ca7dffe11ee67e6a7a9
sanjai2002 <sanjaihari2002@gmail.com>
2024-03-25 17:01:32 +0530

    Added Back End Repo

---
+++ Models/Purchase.cs
@@ -0,0 +1,16 @@
+﻿namespace InventoryManagementSystem.Models
+{
+    public class Purchase
+    {
+        public int PurchaseId { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public Retailer? Retailer { get; set; }
+        public int Retailerid { get; set; }
+        public SuperProduct? SuperProduct { get; set; }
+        public int SuperProductId { get; set; }
+        public int Count { get; set; }
+        public string? BillId { get; set; }
+        public int ProductAmount { get; set; }
+  
+    }
+}
